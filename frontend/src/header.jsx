487678b00d831bbb509dc68aee9314dfb9f676b4
Judiel Legaspina <legaspinajudiel10@gmail.com>
2025-06-06 21:18:38 +0800

    Initial Commit Header (not responsive)

--- frontend/src/header.jsx
+++ frontend/src/header.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="w-full h-24 bg-white shadow-[0px_4px_10px_rgba(0,0,0,0.25)] fixed top-0 z-50">
+    <header className="w-full h-15 bg-white shadow-[0px_4px_10px_rgba(0,0,0,0.25)] fixed top-0 z-50">
       <div className="container mx-auto h-full flex items-center px-4">
         {/* Logo */}
-        <div className="flex-shrink-0 mr-8">
+        <div className="flex-shrink-0 mr-5 ml-3">
           <img 
-            className="w-20 h-20" 
+            className="w-10 h-10" 
             src="/logoWithoutLabel.png" 
             alt="DOST Logo" 
           />
         </div>
         
         {/* Title */}
-        <h1 className="text-3xl font-bold text-sky-700 font-open-sans">
+        <h1 className="text-xl font-bold text-sky-700 font-open-sans">
           DEPARTMENT OF SCIENCE AND TECHNOLOGY 1
         </h1>
       </div>
