c2b9a24932db6d7d6fda86e0246733ed2ee941f0
Judiel Legaspina <legaspinajudiel10@gmail.com>
2025-06-07 17:48:22 +0800

    add incoming component

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/login.jsx';
 import Dashboard from './Components/dashboard.jsx';
+import Incoming from './Components/incoming.jsx';
 
 function Index() {
   return (
@@ -8,6 +9,7 @@ function Index() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/incoming" element={<Incoming />} />
       </Routes>
     </Router>
   );
